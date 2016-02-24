42fd60591c60d03e3a7c8a05cd8c7a0091a7ca46
DonPatus <patus1112@gmail.com>
2016-02-24 16:40:54 +0100

    Added files via upload
    Moje zoo - Patryk Adamczak

---
+++ DonPatus.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+
+
+class zwierze
+{
+public:
+    int8_t masa;
+    virtual void daj_glos() = 0; //klasa abstrakcyjna, niedostepne do swiata zewnetrznego
+};
+
+int main()
+{
+
+    class kot;
+    cout << "Hello World";
+    return 0;
+}
